1e834ec53e19eb41e529bc4f7d87c259acf35af4
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-14 14:41:39 +0100

    serializers

--- template_service/templates/serializers.py
+++ template_service/templates/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import Template
+from .services import extract_placeholders
 
 class TemplateVariableExtractionSerializer(serializers.Serializer):
     name = serializers.CharField(read_only=True)
 
 class TemplateCompileSerializer(serializers.Serializer):
-    variables = serializers.DictField(child=serializers.CharField())
+    variables = serializers.DictField(child=serializers.JSONField())
 
 class TemplateResponseSerializer(serializers.ModelSerializer):
     required_variables = serializers.SerializerMethodField()
@@ -15,7 +16,6 @@ class TemplateResponseSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'subject', 'html_body', 'text_body', 'required_variables', 'is_active']
 
     def get_required_variables(self, obj):
-        from .services import extract_placeholders
         vars_set = extract_placeholders(obj.html_body)
         if obj.text_body:
             vars_set.update(extract_placeholders(obj.text_body))
