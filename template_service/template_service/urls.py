b17e30624e9e95312d3e4b2fc6d2039cff164a77
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-14 15:15:24 +0100

    registered templates on admin

--- template_service/template_service/urls.py
+++ template_service/template_service/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('templates_app.urls')),
+    path('api/v1/', include('templates.urls')),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('health/', lambda request: Response({"status": "healthy"}), name='health'),
 ]
