f3e391a267e4f08e4394f62b27fe15dca17c82d4
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-21 10:49:07 +0100

    fixed the template url to handle response
    # Conflicts:
    #	template_service/template_service/settings.py

--- template_service/templates/urls.py
+++ template_service/templates/urls.py
@@ -5,7 +5,7 @@ from .views import EmailTemplateViewSet
 router = DefaultRouter()
 router.register(r'templates', EmailTemplateViewSet, basename='template')
 
-app_name = 'templates_app'
+app_name = 'templates'
 
 urlpatterns = [
     path('', include(router.urls)),
