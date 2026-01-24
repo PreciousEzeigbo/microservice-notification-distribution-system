ee1bf26e0ecd5deb4f5e7c7932608da5e9ced012
Precios Ezeigbo <preciousezeigo81@gmail.com>
2026-01-24 21:50:48 +0100

    Fix URL namespace in tests from templates_app to templates

--- template-service/template_service/tests/test_templates.py
+++ template-service/template_service/tests/test_templates.py
@@ -26,7 +26,7 @@ class TestEmailTemplateAPI:
     
     def test_create_template(self, api_client):
         """Test creating a new template"""
-        url = reverse('templates_app:template-list')
+        url = reverse('templates:template-list')
         data = {
             'name': 'new_template',
             'template_type': 'welcome',
@@ -41,7 +41,7 @@ class TestEmailTemplateAPI:
     
     def test_get_template(self, api_client, sample_template):
         """Test retrieving a template"""
-        url = reverse('templates_app:template-detail', kwargs={'name': sample_template.name})
+        url = reverse('templates:template-detail', kwargs={'name': sample_template.name})
         response = api_client.get(url)
         
         assert response.status_code == status.HTTP_200_OK
@@ -49,7 +49,7 @@ class TestEmailTemplateAPI:
     
     def test_validate_template(self, api_client, sample_template):
         """Test template validation with variables"""
-        url = reverse('templates_app:template-validate', kwargs={'name': sample_template.name})
+        url = reverse('templates:template-validate', kwargs={'name': sample_template.name})
         data = {
             'variables': {
                 'variable': 'test',
@@ -63,7 +63,7 @@ class TestEmailTemplateAPI:
     
     def test_extract_placeholders(self, api_client, sample_template):
         """Test extracting placeholders from template"""
-        url = reverse('templates_app:template-placeholders', kwargs={'name': sample_template.name})
+        url = reverse('templates:template-placeholders', kwargs={'name': sample_template.name})
         response = api_client.get(url)
         
         assert response.status_code == status.HTTP_200_OK
@@ -72,7 +72,7 @@ class TestEmailTemplateAPI:
     
     def test_health_check(self, api_client):
         """Test health check endpoint"""
-        url = reverse('templates_app:template-health')
+        url = reverse('templates:template-health')
         response = api_client.get(url)
         
         assert response.status_code == status.HTTP_200_OK
